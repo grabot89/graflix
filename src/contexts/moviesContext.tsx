175a35fd2ed7ef829567415ab047411315e06778
Grainne Moran <20104297@mail.wit.ie>
2024-07-29 21:50:28 +0100

    First commit

---
+++ src/contexts/moviesContext.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useCallback } from "react";
+import { BaseMovieProps, Review } from "../types/interfaces";
+
+interface MovieContextInterface {
+    favourites: number[];
+    addToFavourites: ((movie: BaseMovieProps) => void);
+    removeFromFavourites: ((movie: BaseMovieProps) => void);
+    addReview: ((movie: BaseMovieProps, review: Review) => void);  // NEW
+    playlist: number[];
+    addToPlaylist: ((movie: BaseMovieProps) => void);
+}
+
+const initialContextState: MovieContextInterface = {
+    favourites: [],
+    addToFavourites: () => {},
+    removeFromFavourites: () => {},
+    addReview: (movie, review) => { movie.id, review},  // NEW
+    playlist: [],
+    addToPlaylist: () => {},
+};
+
+export const MoviesContext = React.createContext<MovieContextInterface>(initialContextState);
+
+const MoviesContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+    const [favourites, setFavourites] = useState<number[]>([]);
+    const [myReviews, setMyReviews] = useState<Review[]>( [] )  // NEW
+    const [playlist, setPlaylist] = useState<number[]>([]);
+
+    const addToFavourites = useCallback((movie: BaseMovieProps) => {
+        setFavourites((prevFavourites) => {
+            if (!prevFavourites.includes(movie.id)) {
+                return [...prevFavourites, movie.id];
+            }
+            return prevFavourites;
+        });
+    }, []);
+
+    const removeFromFavourites = useCallback((movie: BaseMovieProps) => {
+        setFavourites((prevFavourites) => prevFavourites.filter((mId) => mId !== movie.id));
+    }, []);
+
+    const addReview = (movie:BaseMovieProps, review: Review) => {   // NEW
+        setMyReviews( {...myReviews, [movie.id]: review } )
+    };
+
+    const addToPlaylist = useCallback((movie: BaseMovieProps) => {
+        setPlaylist((prevPlaylist) => {
+            if (!prevPlaylist.includes(movie.id)) {
+                return [...prevPlaylist, movie.id];
+            }
+            return prevPlaylist;
+        });
+    }, []);
+
+    return (
+        <MoviesContext.Provider
+            value={{
+                favourites,
+                addToFavourites,
+                removeFromFavourites,
+                addReview,
+                playlist,
+                addToPlaylist,
+            }}
+        >
+            {children}
+        </MoviesContext.Provider>
+    );
+};
+
+export default MoviesContextProvider;
